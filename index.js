9af6affc1e082f09be6378c29eaa6acb19b506fa
wuyupei <495174699@qq.com>
2023-12-17 15:01:39 +0800

    init2

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express")
 const mysql = require('mysql2');
 const cors = require("cors")
+const nodemailer = require('nodemailer')
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.qq.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: '495174699@qq.com',
+    pass: "vilfljggjgoccbef",
+  }
+})
 
 const app = express()
 app.use(cors())
@@ -29,6 +40,37 @@ app.post("/submit", (req, res) => {
   console.log('---');
   connection.execute("INSERT INTO users (phone, book_id, create_date, update_date) VALUES (?, ?, ?, ?)", [phone, book_id, new Date(), new Date()], (err, result) => {
     res.send({ code: 200, success: 200 })
+
+    const receiverone = {
+      // 发件人 邮箱  '昵称<发件人邮箱>'
+      from: `495174699@qq.com`,
+      // 主题
+      subject: '通知',
+      // 收件人 的邮箱 可以是其他邮箱 不一定是qq邮箱
+      to: '495174699@qq.com',
+      // 可以使用html标签
+      html: `掘金小册bookId：${book_id} 联系方式：${phone}，请尽快处理`
+    }
+
+    const receivertwo = {
+      // 发件人 邮箱  '昵称<发件人邮箱>'
+      from: `495174699@qq.com`,
+      // 主题
+      subject: '通知',
+      // 收件人 的邮箱 可以是其他邮箱 不一定是qq邮箱
+      to: '2456635159@qq.com',
+      // 可以使用html标签
+      html: `掘金小册bookId：${book_id} 联系方式：${phone}，请尽快处理`
+    }
+
+    transporter.sendMail(receiverone, (error, info) => {
+      transporter.close()
+    })
+
+    transporter.sendMail(receivertwo, (error, info) => {
+      transporter.close()
+    })
+
   })
 })
 
